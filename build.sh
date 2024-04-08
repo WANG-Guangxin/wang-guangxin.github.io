c71071f27e5acd899fe1eefd6c8e6adee5911987
Your Name <you@example.com>
2024-04-08 22:57:18 +0800

    Update at 2024-04-08 22:57:18 with changes: "build.sh"

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 python3 uptime.py
 source siteenv
-envsubst < "./index.md" > "./source/Sites/index.md"
+envsubst < "./index.md" > "./source/sites/index.md"
 hexo generate
